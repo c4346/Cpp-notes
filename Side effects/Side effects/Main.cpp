bae35bcab2ba4f7c79346771e82d3e6bf20869fe
c4346 <53945103+c4346@users.noreply.github.com>
2021-01-25 13:46:26 -0500

    Update Main.cpp

--- Side effects/Side effects/Main.cpp
+++ Side effects/Side effects/Main.cpp
@@ -4,13 +4,13 @@ int add(int x, int y) {
 	return x + y;
 
 }
-//side effects are	cause by a function doing anything that persists it's lifetime
+//side effects are caused by a function doing anything that persists it's lifetime
 int main() {
 
 	int x{ 5 };
 
 	int value = add(x, ++x); //is it 5+6, or 5+5?
-	//evaluates to either 11, or 12; depending on the compiler used
-	std::cout << value; //visual c++ evaluates this to 12, beceause it evaluates from left to right
+	//evaluates to either 10, or 11; depending on the compiler used
+	std::cout << value; //visual c++ evaluates this to 11, beceause it evaluates from left to right
 	//To avoid side effects, only use a variable once in a given statemen
 }
